0f13abcb1e72a88a28f40e1cd451345a1f715039
SeongHo-Jeong <141135747+jshstar@users.noreply.github.com>
2024-02-23 19:54:08 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int yearT = scanner.nextInt();
+		int difference = 543;
+		System.out.println(yearT - difference);
+		scanner.close();
+	}
+}
