b9e495db3b52b4ab72a564655bc10b58bcc19afb
SeongHo-Jeong <141135747+jshstar@users.noreply.github.com>
2024-03-24 18:08:16 +0900

    [Bronze II] Title: 단어의 개수, Time: 476 ms, Memory: 30932 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -5,7 +5,6 @@ public class Main {
  
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
- 
 		String S = in.nextLine();
 		in.close();
 		StringTokenizer st = new StringTokenizer(S," ");
