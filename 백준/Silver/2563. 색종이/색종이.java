0e7e0a96d848d0576ef384e71f269e11315a259d
SeongHo-Jeong <141135747+jshstar@users.noreply.github.com>
2024-04-13 20:29:54 +0900

    [Silver V] Title: 색종이, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int totalArea = 0;
+		boolean[][] paper = new boolean[100][100];
+ 		for (int i = 0; i < n; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			for (int j = x; j < x + 10; j++) {
+				for (int k = y; k < y + 10; k++) {
+					if (!paper[j][k]) {
+						paper[j][k] = true;
+						totalArea++;
+					}
+				}
+			}
+		}
+		System.out.println(totalArea);
+	}
+}
