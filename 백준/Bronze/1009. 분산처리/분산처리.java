452cce555a25b0267adbd7fc15c08e7370f7ea8d
SeongHo-Jeong <141135747+jshstar@users.noreply.github.com>
2024-06-27 17:14:08 +0900

    [Bronze II] Title: 분산처리, Time: 1032 ms, Memory: 17400 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (a % 10 == 0) {
+				bw.write(10 + "\n");
+				continue;
+			}
+			int num = a % 10;
+			for (int j = 1; j < b; j++) {
+				num = (num * a) % 10;
+			}
+			bw.write(num + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
+
