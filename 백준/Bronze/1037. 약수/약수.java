37160a3eefa53592a30bc9a6634551ab4123593e
SeongHo-Jeong <141135747+jshstar@users.noreply.github.com>
2024-08-10 22:41:05 +0900

    [Bronze I] Title: 약수, Time: 128 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			min =  Math.min(min, arr[i]);
+			max = Math.max(max, arr[i]);
+		}
+		System.out.println(min * max);
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
+
