a4006669af4e0e6fef7a098fe436a7f72c93467c
SeongHo-Jeong <141135747+jshstar@users.noreply.github.com>
2024-10-07 21:12:25 +0900

    [Silver II] Title: 주식, Time: 1040 ms, Memory: 316604 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,31 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int N;
-        StringTokenizer st;
-        int[] arr;
-        while (T-- > 0) {
-            N = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            arr = new int[N];
-            for (int i = 0; i < N; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
-            long answer = 0;
-            int max = Integer.MIN_VALUE;
-            for (int i = N - 1; i >= 0; i--) {
-                if (max < arr[i]) {
-                    max = arr[i];
-                }
-                answer += max - arr[i];
-            }
-            System.out.println(answer);
-        }
-    }
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int t = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int[] stoke;
+		for (int i = 0; i < t; i++) {
+
+			int n = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			stoke = new int[n];
+			int max = Integer.MIN_VALUE;
+			long result = 0;
+
+			for (int j = 0; j < n; j++) {
+				stoke[j] = Integer.parseInt(st.nextToken());
+			}
+			for (int j = n - 1; j >= 0; j--) {
+				if (max < stoke[j]) {
+					max = stoke[j];
+				} 
+				result += max - stoke[j];
+			}
+			bw.write(result + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
 }
