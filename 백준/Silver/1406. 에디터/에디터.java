487d9d9e40f88d971da7bf3c0291627bf9a3072d
SeongHo-Jeong <141135747+jshstar@users.noreply.github.com>
2024-10-08 16:34:44 +0900

    [Silver II] Title: 에디터, Time: 612 ms, Memory: 126956 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -6,40 +6,48 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		String n = br.readLine();
-		sb.append(n);
-		int cursor = sb.length();
+		Stack<String> stack1 = new Stack<>();
+		Stack<String> stack2 = new Stack<>();
+		String s = br.readLine();
+
+		for (int i = 0; i < s.length(); i++) {
+			stack1.push(String.valueOf(s.charAt(i)));
+		}
+
 		int m = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		for(int i = 0; i < m; i++){
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			String s = st.nextToken();
-			switch(s){
-				case "L" :
-					if(cursor > 0){
-						cursor--;
+			String command = st.nextToken();
+			switch (command) {
+				case "L":
+					if (!stack1.isEmpty()) {
+						stack2.push(stack1.pop());
 					}
 					break;
-				case "D" :
-					if(cursor < sb.length()){
-						cursor++;
+				case "D":
+					if (!stack2.isEmpty()) {
+						stack1.push(stack2.pop());
 					}
 					break;
-				case "B" :
-					if(cursor > 0){
-						cursor--;
-						sb.deleteCharAt(cursor);
+				case "B":
+					if (!stack1.isEmpty()) {
+						stack1.pop();
 					}
 					break;
-				case "P" :
-					sb.insert(cursor, st.nextToken());
-					cursor++;
+				case "P":
+					stack1.push(st.nextToken());
+					break;
+				default:
 					break;
 
 			}
 		}
-		bw.write(sb + "\n");
+		while (!stack1.isEmpty())
+			stack2.push(stack1.pop());
+
+		while (!stack2.isEmpty())
+			bw.write(stack2.pop());
 		bw.flush();
 		bw.close();
 		br.close();
